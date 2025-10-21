3c8ec7202c08080fa0b11e323461fe8de05f7621
Mykola Shchyrba <77409752+Kolya-13@users.noreply.github.com>
2025-10-22 02:48:24 +0300

    added homework

---
+++ arifmetics.js
@@ -0,0 +1,38 @@
+
+let a = 5;
+let b = 10;
+
+
+console.log(`Сума чисел:  a + b = ${a + b}`);
+console.log(`Різниця чисел: a - b = ${a - b}`);
+console.log(`Добуток чисел a * b = ${a * b}`);
+console.log(`Ділення чисел a / b = ${a / b}`);
+
+
+
+let str1 = "5";
+let str2 = "2";
+
+
+console.log("Сума рядків:", str1 + str2);   
+console.log("Різниця рядків:", str1 - str2); 
+console.log("Добуток рядків:", str1 * str2); 
+console.log("Ділення рядків:", str1 / str2); 
+
+
+console.log("Число + рядок:", a + str1); 
+console.log("Число - рядок:", a - str1);  
+
+
+let bool1 = true;  
+let bool2 = false; 
+
+console.log("Число + буль:", a + bool1); 
+console.log("Буль + рядок:", bool1 + str1);
+
+
+let x = null;     
+let y = undefined;  
+
+console.log("Число + null:", a + x);       
+console.log("Число + undefined:", a + y);  
---
+++ logical.js
@@ -0,0 +1,34 @@
+
+
+
+let num = 10;
+
+let str = "10";
+
+let isActive = true;
+
+let empty = null;
+let notSet;
+
+
+console.log("num == str:", num == str);         
+console.log("num === str:", num === str);       
+console.log("num != str:", num != str);         
+console.log("num !== str:", num !== str);       
+console.log("num > 5:", num > 5);              
+console.log("str < 20:", str < 20);             
+
+
+console.log("isActive && num > 5:", isActive && num > 5); 
+console.log("isActive || num < 5:", isActive || num < 5); 
+console.log("!isActive:", !isActive);                     
+
+
+
+console.log("empty == null:", empty == null);             
+console.log("notSet === undefined:", notSet === undefined); 
+console.log("empty === undefined:", empty === undefined); 
+
+
+console.log("(num > 5 && str === '10'):", (num > 5 && str === "10")); 
+console.log("(num < 5 || isActive === false):", (num < 5 || isActive === false))
